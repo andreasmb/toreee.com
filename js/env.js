a7401c2bb79e45aadaf831f996840ab3099a0618
andreasmb <andreasmb@users.noreply.github.com>
2024-05-18 23:07:05 +0000

    deploy: a4c28c82d62bd08c4aeeb3f0d7c2dec5b91eb764

--- js/env.js
+++ js/env.js
@@ -1,2 +1,2 @@
-window.env = { AIRTABLE_API_KEY: 'patHYTNk8NQFCZLkK.63a6c67d2585ff4222dbb7f735692a8415de7e07c27c93a9c725239f11ad6de7', AIRTABLE_BASE_ID: 'appWjWl1OCktfyHo0' };
+window.env = { AIRTABLE_API_KEY: 'patHYTNk8NQFCZLkK.03500e17d167d8e96599060858dd9e1287d0dd4c33c450ab60645d575a0a204b', AIRTABLE_BASE_ID: 'appWjWl1OCktfyHo0' };
 console.log('Environment Variables:', window.env);
